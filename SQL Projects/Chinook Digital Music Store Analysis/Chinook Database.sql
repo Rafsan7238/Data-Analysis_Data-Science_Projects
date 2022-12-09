e439085ef2a0ed77a745c6847313aee2c9d7c2d5
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2022-12-09 19:41:33 +0600

    Created the database

---
+++ SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
@@ -0,0 +1,29 @@
+/*******************************************************************************
+   Chinook Database - Version 1.4
+   Script: Chinook_SqlServer.sql
+   Description: Creates and populates the Chinook database.
+   DB Server: SqlServer
+   Author: Luis Rocha
+   License: http://www.codeplex.com/ChinookDatabase/license
+********************************************************************************/
+
+/*******************************************************************************
+   Drop database if it exists
+********************************************************************************/
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Chinook')
+BEGIN
+	ALTER DATABASE [Chinook] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+	ALTER DATABASE [Chinook] SET ONLINE;
+	DROP DATABASE [Chinook];
+END
+
+GO
+
+/*******************************************************************************
+   Create database
+********************************************************************************/
+CREATE DATABASE [Chinook];
+GO
+
+USE [Chinook];
+GO
