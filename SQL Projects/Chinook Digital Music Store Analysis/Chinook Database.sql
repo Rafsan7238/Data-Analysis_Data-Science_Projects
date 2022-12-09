55455e6b8e58eddfaa30058e01b8ca0649da9a97
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2022-12-09 19:45:28 +0600

    Added employee table

--- SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
+++ SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
@@ -64,3 +64,23 @@ CREATE TABLE [dbo].[Customer]
     CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([CustomerId])
 );
 GO
+CREATE TABLE [dbo].[Employee]
+(
+    [EmployeeId] INT NOT NULL IDENTITY,
+    [LastName] NVARCHAR(20) NOT NULL,
+    [FirstName] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(30),
+    [ReportsTo] INT,
+    [BirthDate] DATETIME,
+    [HireDate] DATETIME,
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60),
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeId])
+);
+GO
