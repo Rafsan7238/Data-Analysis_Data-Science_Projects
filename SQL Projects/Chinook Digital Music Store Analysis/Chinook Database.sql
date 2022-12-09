68ea1a279c99aeb285184a5615989e5af8d5acd7
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2022-12-09 19:44:07 +0600

    Added artist table

--- SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
+++ SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
@@ -39,3 +39,10 @@ CREATE TABLE [dbo].[Album]
     CONSTRAINT [PK_Album] PRIMARY KEY CLUSTERED ([AlbumId])
 );
 GO
+CREATE TABLE [dbo].[Artist]
+(
+    [ArtistId] INT NOT NULL IDENTITY,
+    [Name] NVARCHAR(120),
+    CONSTRAINT [PK_Artist] PRIMARY KEY CLUSTERED ([ArtistId])
+);
+GO
