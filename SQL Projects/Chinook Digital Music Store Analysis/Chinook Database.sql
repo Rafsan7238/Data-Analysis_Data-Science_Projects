dc58f3bbd5dcdd8fab06752306a7a390b69d6d94
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2022-12-09 19:45:06 +0600

    Added customer table

--- SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
+++ SQL Projects/Chinook Digital Music Store Analysis/Chinook Database.sql
@@ -46,3 +46,21 @@ CREATE TABLE [dbo].[Artist]
     CONSTRAINT [PK_Artist] PRIMARY KEY CLUSTERED ([ArtistId])
 );
 GO
+CREATE TABLE [dbo].[Customer]
+(
+    [CustomerId] INT NOT NULL IDENTITY,
+    [FirstName] NVARCHAR(40) NOT NULL,
+    [LastName] NVARCHAR(20) NOT NULL,
+    [Company] NVARCHAR(80),
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60) NOT NULL,
+    [SupportRepId] INT,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([CustomerId])
+);
+GO
